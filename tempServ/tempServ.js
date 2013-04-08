910c0dad878a8bdb2649fee8b4d7bd41f3994ca2
ubergesundheit <ubergesundheit@googlemail.com>
2013-04-08 22:35:28 +0200

    package.json, config, prepared for upcomming couchdb changes

--- tempServ/tempServ.js
+++ tempServ/tempServ.js
@@ -1,24 +1,27 @@
 var express = require('express');
-var http = require('http');
 var app = express();
-var MongoClient = require('mongodb').MongoClient, Server = require('mongodb').Server;
-var temps = null;
+var config = require('./config.json');
 
+
+//var MongoClient = require('mongodb').MongoClient, Server = require('mongodb').Server;
+//var temps = null;
+/*
 var mongoClient = new MongoClient(new Server('localhost', 27017));
 mongoClient.open(function(err, mongoClient) {
   var dbTemps = mongoClient.db('temps');
   temps = dbTemps.collection('temps');
-
+*/
 
 //express stuff..
+
 app.get('/', function(req, res){
   res.send('/all	<-	get all data<br>'+
   '/now	<-	get current temperature<br>'+
   '/today	<-	get data of today<br>'+
   '/last_hour	<-	get data of last hour<br>'+
   '/graph 	<-	get graph with all data<br>'+
-  '/licht	<-	switch light on/off');
 });
+
 
 app.get('/files/:file', function(req, res){
     res.sendfile('files/'+req.params.file);
@@ -28,21 +31,6 @@ app.get('/graph', function(req, res){
     res.sendfile('files/graph.html');  
 });
 
-app.get('/licht', function(req, res){
-    var post = http.request({ host: '192.168.178.235', port: '80', path: '/leds.cgi?led=1', method: 'POST'}, function (res) {});
-    post.end();
-    var get = http.get({ host: '192.168.178.235', port: '80', path: '/status.xml'}, function(getres) { 
-	var xmlresponse = '';
-	  getres.on('data', function (chunk) {
-	    xmlresponse += chunk;
-          });
-	  getres.on('end', function(){
-	    res.set('Content-Type', 'text/plain');
-	    res.send(xmlresponse.substr(12,14));
-	  });
-    });
-    //res.send('success');
-});
 
 app.get('/all', function(req, res){
   temps.find({},{_id:false}).sort({ date: 1}).toArray(function(err, items) {
@@ -94,7 +82,6 @@ app.get('/last_hour', function(req, res){
     res.json(items);
   });
 });
-
 
   //mongoClient.close();
 });
